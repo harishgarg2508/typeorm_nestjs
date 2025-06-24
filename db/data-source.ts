06da32108af7b76fa8365401021d8cb4c266635a
Harish Garg <rishusharma123321@gmail.com>
2025-06-24 09:34:46 +0530

    changes in datasource

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,25 +1,22 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
-import { config } from 'dotenv';
 import { ConfigService } from '@nestjs/config';
 import { User } from 'src/entities/user/User';
-
-config();
 
 const configService = new ConfigService();
 
 const rawDataSourceOptions = {
-            type: (configService.get<string>('DATABASE_TYPE') as DataSourceOptions['type']) || 'mysql',
-            host: configService.get<string>('DATABASE_HOST'),
-            port: Number(configService.get<string>('DATABASE_PORT')),
-            username: configService.get<string>('DATABASE_USERNAME'),
-            password: configService.get<string>('DATABASE_PASSWORD'),
-            database: configService.get<string>('DATABASE_NAME'),
-            synchronize: false,
-            // autoLoadEntities: true,
-            logging:true,
-            migrationsTableName:'typeorm_migrations',
-            migrationsRun:false,
-            entities: [User],
+    type: (configService.get<string>('DATABASE_TYPE') as DataSourceOptions['type']),
+    host: configService.get<string>('DATABASE_HOST'),
+    port: Number(configService.get<string>('DATABASE_PORT')),
+    username: configService.get<string>('DATABASE_USERNAME'),
+    password: configService.get<string>('DATABASE_PASSWORD'),
+    database: configService.get<string>('DATABASE_NAME'),
+    synchronize: false,
+    // autoLoadEntities: true, // if using this then no need to pass entities seperately
+    logging: true,
+    migrationsTableName: 'typeorm_migrations',
+    migrationsRun: false,
+    entities: [User],
 };
 
 export const dataSourceOptions = rawDataSourceOptions as DataSourceOptions;
